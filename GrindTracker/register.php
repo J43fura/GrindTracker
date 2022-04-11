483cf5ec46962dd699f9e89fccf6da9eddca4d24
J43fura <mehdi.jaafourag@gmail.com>
2022-04-11 01:31:37 +0100

    PHP -> SQL
    register.php:
    el port 7atitou 3307 5atr 3306 mte3i mst3ml.
    el default 3306.
    
    *extension: Open PHP/HTML/JS In Browser
    **el project files yet7attou fel "C:\xampp\htdocs" bch ynjm ya9rahom el php

--- GrindTracker/register.php
+++ GrindTracker/register.php
@@ -1,59 +1,37 @@
-<?php 
-&session_start
-$con = mysqli_connect('localhost','root','');
-mysqli_select_db(&con,'grindtracker')
+<?php
 
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-if (!empty($username) ||!empty($username)){
-$host="localhost";
-&dbUsername="root";
-&dbPassword="";
-&dbname="grindtracker";
-
-&con = new msqli($host,$dbUsername,$dbPassword,$dbname);
-
-if(mysqli_connect_error()){
-	die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+// Create connection
+$conn = new mysqli ("localhost","root","admin","grindtracker","3307");
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
 }
 else{
-	$SELECT = "SELECT username From register Where username = ? Limit 1 "
-	&INSERT = "INSERT Into register (username, password) values(?, ?)"
-
-	//Prepare statement
-	
-	$stmt = $conn->prepare($Select);
-	$stmt->bind_param("s", $username);
-	$stmt->execute();
-	$stmt->bind_result($username);
-	$stmt->store_result();
-	$stmt->fetch();
-	$rnum = $stmt->num_rows;
-
-	if ($rnum == 0) {
-		$stmt->close();
-		$stmt = $conn->prepare($INSERT);
-		$stmt->bind_param("ss",$username, $password);
-		if ($stmt->execute()) {
-			echo "New record inserted sucessfully.";
-		}
-		else {
-			echo $stmt->error;
-		}
-	}
-	else {
-		echo "Someone already registers using this username.";
-	}
-	$stmt->close();
-	$conn->close();
+$sql = "INSERT INTO register(username, password)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
 }
 }
-else {
-echo "All field are required.";
+else{
+echo "Password should not be empty";
 die();
 }
-else {
-echo "Submit button is not set";
 }
+else{
+echo "Username should not be empty";
+die();
+}
+
 ?>
