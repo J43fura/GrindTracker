0040dbe236aab89d178f6f0c9c0994cf52088945
Jaafoura <73950268+J43fura@users.noreply.github.com>
2022-11-26 17:05:52 +0100

    CREATE and USE database
    CREATE DATABASE grindtracker;
    USE grindtracker;

--- GrindTracker/Adds/grindtracker.sql
+++ GrindTracker/Adds/grindtracker.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `grindtracker`
 --
+CREATE DATABASE grindtracker;
+USE grindtracker;
+
 
 -- --------------------------------------------------------
 
