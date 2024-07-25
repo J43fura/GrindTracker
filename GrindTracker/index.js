7797299803d2a24fb6527ac706c890ef3d409a27
Jaafoura <73950268+J43fura@users.noreply.github.com>
2024-07-25 19:00:11 +0100

    Update index.js

--- GrindTracker/index.js
+++ GrindTracker/index.js
@@ -1,9 +1,9 @@
 function togglePopup() {
   document.getElementById("popup-1").classList.toggle("active");
-  document.body.style.overflow(hidden);
+  document.body.style.overflow = "hidden";
 }
 
 function togglePopupVer() {
   document.getElementById("popup-Ver").classList.toggle("active");
-  document.body.style.overflow(hidden);
+  document.body.style.overflow = "hidden";
 }
